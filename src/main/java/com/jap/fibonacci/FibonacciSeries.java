d63ea529c2acdcc5c2933408b52605d30247f52f
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-02 12:50:52 +0530

    Sprint4

--- src/main/java/com/jap/fibonacci/FibonacciSeries.java
+++ src/main/java/com/jap/fibonacci/FibonacciSeries.java
@@ -4,17 +4,32 @@ public class FibonacciSeries
 {
     // Write the logic in the method to generate the next number in the Fibonacci series
     public long fibonacci(long number) {
-        return -2;
+
+        if(number < 0){
+            return -1;
+        }
+        else if(number <= 1){
+            return number;
+        }
+        else {
+            return fibonacci(number-1) + fibonacci(number - 2);
+        }
+
+
     }
     // Print the series inside the method
     public void printFibonacciSeries(long number){
+        for (int i = 0; i < number; i++) {
+
+            System.out.print(" " + fibonacci(i));
+        }
 
     }
     public static void main( String[] args )
     {
         FibonacciSeries fib = new FibonacciSeries();
         // Call the  printFibonacciSeries() method and pass the value.
-
+            fib.printFibonacciSeries(9);
 
     }
 }
